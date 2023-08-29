05c58e14c67d09cd924fa1080477e138ebc2f5ab
aditya23ds <aditya.singh@msds.christuniversity.in>
2023-08-29 07:58:01 +0530

    second.ipynb

---
+++ Untitled-1.py
@@ -0,0 +1,11 @@
+listex = ["Abin", "Suraj", "Aditya"]
+listex.append("Manish")
+listex.append("Siddharth")
+
+name = input("Enter A name")
+
+if name in listex == "q" :
+   print("END OF PROCESS")
+
+elif name in listex == name:
+    print("Name already exists")
